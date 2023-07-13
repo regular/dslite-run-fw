66f8ff45836a6cb8d3c62c27d0eb462186648dd5
Jan Bölsche <regular.gonzales@googlemail.com>
2023-07-13 10:50:29 +0200

    Detect EOT (ASCII 0x04) in stdout of firmware and parse reason for exit

--- session.js
+++ session.js
@@ -9,86 +9,127 @@ module.exports = async function session(ds, target, program, opts) {
     ds.waitForEvent({
       good: debugMain
     })
-    const {cores} = await ds.configure(target)
-    const core = await ds.createSubModule(cores[0])
-    
-    core.waitForEvent({
-      good: debugCore
-    })
+    try {
+      const {cores} = await ds.configure(target)
+      const core = await ds.createSubModule(cores[0])
+      
+      core.waitForEvent({
+        good: debugCore
+      })
 
-    await core.cio.willHandleInput(true)
-    await core.targetState.connect()
-    await core.settings.set({
-      AutoRunToLabelName: "main"
-    })
-    await core.symbols.loadProgram(program)
+      await core.cio.willHandleInput(true)
+      await core.targetState.connect()
+      await core.settings.set({
+        AutoRunToLabelName: "main"
+      })
+      await core.symbols.loadProgram(program)
 
-    await core.waitForEvent({
-      good: ({data, event}) => event == 'targetState.changed' && data.description == 'Suspended - H/W Breakpoint',
-      timeout: 10 * 1000,
-    }).catch(err=>{
-      throw new Error('Device did not enter expected target state: ' + err.message)
-    })
+      await core.waitForEvent({
+        good: ({data, event}) => event == 'targetState.changed' && data.description == 'Suspended - H/W Breakpoint',
+        timeout: 10 * 1000,
+      }).catch(err=>{
+        throw new Error('Device did not enter expected target state: ' + err.message)
+      })
 
-    const stderr = process.stderr
-    let stdout = process.stdout
-    if (opts.numLines) {
-      stdout = captureLines(resolve, opts)
+      const stdout = captureLines(opts, (err, lines)=>{
+        if (!err) return resolve(lines)
+        reject(err)
+      }, opts)
       stdout.pipe(process.stdout)
-    }
+      const output = Output(stdout, process.stderr)
 
-    function output(type, message) {
-      if (type=='stdout') stdout.write(Buffer.from(message))
-      if (type=='stderr') stderr.write(message)
-    }
+      const outputEvents = core.waitForEvent({
+        good: ({data, event}) => {
+          const {message, type} = data
+          if (event == 'cio.output') {
+            output(type, message)
+          }
+          return false // don't resolve the promise
+        },
+        timeout: 6 * 1000,
+      }).catch(reject)
 
-    const outputEvents = core.waitForEvent({
-      good: ({data, event}) => {
-        const {message, type} = data
-        if (event == 'cio.output') {
-          output(type, message)
-        }
-        return false // don't resolve the promise
-      },
-      timeout: 6 * 1000,
-    }).catch(reject)
-
-    core.targetState.run()
-    await core.waitForEvent({
-      good: ({data, event}) => event == 'targetState.changed' && data.description == 'Running',
-      timeout: 6 * 1000,
-    })
-    const halted = core.waitForEvent({
-      good: ({data, event}) => event == 'targetState.changed' && data.description == 'Suspended',
-    })
-    halted.then( async ()=>{
-      const stack = await core.callstack.fetch()
-      const pstack = prettyStack(stack)
-      reject(new Error('Target halted:\n' + pstack))
-    })
+      core.targetState.run()
+      await core.waitForEvent({
+        good: ({data, event}) => event == 'targetState.changed' && data.description == 'Running',
+        timeout: 6 * 1000,
+      })
+      const halted = core.waitForEvent({
+        good: ({data, event}) => event == 'targetState.changed' && data.description == 'Suspended',
+      })
+      halted.then( async ()=>{
+        const stack = await core.callstack.fetch()
+        const pstack = prettyStack(stack)
+        reject(new Error('Target halted:\n' + pstack))
+      })
+    } catch(err) {reject(err)}
   })
 }
 
 // -- util
 
-function captureLines(done, opts) {
+function captureLines(opts, done) {
   const lines = []
   let buff = []
-  const s = loner('\n')
+  const s = loner('\n', Buffer.from([0x04]))
   s.on('data', data=>{
     if (!done) return
-    if (data.length !== 1 || data[0] !== '\n'.charCodeAt(0)) {
-      buff.push(data)
-    } else {
+    if (data.length == 1 && data[0] == 0x04) {
+      const exitReason = buff.pop().toString()
+      console.error('exit reason:', exitReason)
+      const err = errorFromExitReason(exitReason, lines)
+      done(err, lines)
+      done = null
+    } else if (data.length == 1 && data[0] == '\n'.charCodeAt(0)) { 
       if (lines == undefined) return
       const line = BufferList(buff).toString('utf8')
       lines.push(line)
       buff = []
       if (done && opts.numLines && lines.length >= opts.numLines) {
-        done(lines)
+        done(null, lines)
         done = null
       }
+    } else {
+      buff.push(data)
     }
   })
   return s
+}
+
+// -- util
+function errorFromExitReason(reason, lines) {
+  if (reason == 'EXCEPTION') {
+    const report = []
+    let line
+    let found = false
+    let done = false
+    do {
+      line = lines.pop()
+      if (found) {
+        if (!line.startsWith('{module#')) {
+          done = true
+          lines.push(line)
+          line = null
+        }
+      }
+      if (line) report.unshift(line)
+      if (!found && line.startsWith('Exception occurred')) found = true
+    } while(!found || !done)
+
+    return new Error('An exception occured:\n' + report.join('\n'))
+  } else if (reason.startsWith('EXIT')) {
+    const code = Number(reason.split(' ')[1])
+    if (code == 0) return null
+    return new Error(`Exit code is ${code}`) 
+  } else if (reason == 'ABORT') {
+    return new Error('abort() was called')
+  }
+  return new Error(reason)
+}
+
+function Output(stdout, stderr) {
+  return function output(type, message) {
+    if (type=='stdout') stdout.write(Buffer.from(message))
+    if (type=='stderr') stderr.write(message)
+  }
 }
